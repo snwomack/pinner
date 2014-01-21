59401fd48a201b99ab87869a294d8569aca11335
Scooter Womack <scooterwomack@gmail.com>
2014-01-21 13:56:04 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinner::Application.config.secret_key_base = '72069c06bbc7aab2e61c2d93eff950dfb0f6e44f00afddf1c6b2e3ca0e69615f32d25ab4478d2dabde5148a35cbb52a7dc553910f36ddf876865a6ea33e93714'
